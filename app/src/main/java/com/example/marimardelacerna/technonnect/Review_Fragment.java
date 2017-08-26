aa38cf72506b42ea282550186a6123a52e02f020
Marimar Dela Cerna <marimardelacerna@Marimars-MacBook-Pro.local>
2017-08-27 01:02:04 +0800

    With Map

--- app/src/main/java/com/example/marimardelacerna/technonnect/Review_Fragment.java
+++ app/src/main/java/com/example/marimardelacerna/technonnect/Review_Fragment.java
@@ -1,23 +1,99 @@
 package com.example.marimardelacerna.technonnect;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * Created by marimardelacerna on 08/05/2017.
  */
-
+//map layout
 public class Review_Fragment extends Fragment {
+
+    private SupportMapFragment mSupportMapFragment;
+
+    GoogleMap googleMap;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        /**
-         * Inflate the layout for this fragment
-         */
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.review_fragment, container, false);
+//        return inflater.inflate(R.layout.review_fragment, container, false);
+
+        View v = inflater.inflate(R.layout.review_fragment, container, false);
+
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return v;
+        }
+        googleMap.setMyLocationEnabled(true);
+        Criteria criteria = new Criteria();
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(provider);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng myPosition = new LatLng(latitude, longitude);
+        Toast.makeText(getContext(), "Hello"+myPosition, Toast.LENGTH_SHORT).show();
+        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapwhere);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.mapwhere, mSupportMapFragment).commit();
+        }
+
+        if (mSupportMapFragment != null) {
+            mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    if (googleMap != null) {
+
+                        googleMap.getUiSettings().setAllGesturesEnabled(true);
+                        LatLng sydney = new LatLng(10.3157, 123.8854);
+//                      -> marker_latlng // MAKE THIS WHATEVER YOU WANT
+
+                        CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(15.0f).build();
+//                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+                        googleMap.addMarker(new MarkerOptions().position(sydney).title("My location"));
+                        googleMap.moveCamera(cameraUpdate);
+
+                    }
+                }
+
+            });
+
+        }
+
+        return v;
     }
-
 }
